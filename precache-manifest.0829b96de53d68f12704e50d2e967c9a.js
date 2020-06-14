6a00706d1bab4c03fbcc922e63f1f166c48a30d5
yusuke akiyama <akiyama@hakoreco.com>
2020-06-15 00:41:30 +0900

    Updates

---
+++ precache-manifest.0829b96de53d68f12704e50d2e967c9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ffc07992a297d565c1536cf1e828013",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9f82192bd81e965b0f53",
+    "url": "/monsters-rolodex/static/css/main.9825f91d.chunk.css"
+  },
+  {
+    "revision": "14d93671136637d7892c",
+    "url": "/monsters-rolodex/static/js/2.e0a80f2d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.e0a80f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f82192bd81e965b0f53",
+    "url": "/monsters-rolodex/static/js/main.3860b739.chunk.js"
+  },
+  {
+    "revision": "03ff305f81a8234a6068",
+    "url": "/monsters-rolodex/static/js/runtime-main.a1e15043.js"
+  }
+]);
